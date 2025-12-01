361b7d8849b111b01a0ec86b01c73f933d7d4f4a
Dimitar <dimitar.mandarliev@gmail.com>
2025-12-01 09:16:36 +0200

    Consuming the CitiesContext

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-import CountriesList from "./components/CountryList";
+import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
@@ -25,7 +25,7 @@ function App() {
             <Route index element={<Navigate replace to="cities" />} />
             <Route path="cities" element={<CityList />} />
             <Route path="cities/:id" element={<City />} />
-            <Route path="countries" element={<CountriesList />} />
+            <Route path="countries" element={<CountryList />} />
             <Route path="form" element={<Form />} />
           </Route>
           <Route path="*" element={<PageNotFound />} />
