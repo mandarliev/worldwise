79c10c3f1baceb0fb122853389a0ef675ef133b4
Dimitar <dimitar.mandarliev@gmail.com>
2025-12-03 22:10:07 +0200

    Deleting a City

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -6,62 +6,62 @@ import Spinner from "./Spinner";
 import BackButton from "./BackButton";
 
 const formatDate = (date) =>
-  new Intl.DateTimeFormat("en", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    weekday: "long",
-  }).format(new Date(date));
+   new Intl.DateTimeFormat("en", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+      weekday: "long",
+   }).format(new Date(date));
 
 function City() {
-  const { id } = useParams();
-  const { getCity, currentCity, isLoading } = useCities();
+   const { id } = useParams();
+   const { getCity, currentCity, isLoading } = useCities();
 
-  useEffect(() => {
-    getCity(id);
-  }, [id]);
+   useEffect(() => {
+      getCity(id);
+   }, [id]);
 
-  const { cityName, emoji, date, notes } = currentCity;
+   const { cityName, emoji, date, notes } = currentCity;
 
-  if (isLoading) return <Spinner />;
+   if (isLoading) return <Spinner />;
 
-  return (
-    <div className={styles.city}>
-      <div className={styles.row}>
-        <h6>City name</h6>
-        <h3>
-          <span>{emoji}</span> {cityName}
-        </h3>
+   return (
+      <div className={styles.city}>
+         <div className={styles.row}>
+            <h6>City name</h6>
+            <h3>
+               <span>{emoji}</span> {cityName}
+            </h3>
+         </div>
+
+         <div className={styles.row}>
+            <h6>You went to {cityName} on</h6>
+            <p>{formatDate(date || null)}</p>
+         </div>
+
+         {notes && (
+            <div className={styles.row}>
+               <h6>Your notes</h6>
+               <p>{notes}</p>
+            </div>
+         )}
+
+         <div className={styles.row}>
+            <h6>Learn more</h6>
+            <a
+               href={`https://en.wikipedia.org/wiki/${cityName}`}
+               target="_blank"
+               rel="noreferrer"
+            >
+               Check out {cityName} on Wikipedia &rarr;
+            </a>
+         </div>
+
+         <div>
+            <BackButton />
+         </div>
       </div>
-
-      <div className={styles.row}>
-        <h6>You went to {cityName} on</h6>
-        <p>{formatDate(date || null)}</p>
-      </div>
-
-      {notes && (
-        <div className={styles.row}>
-          <h6>Your notes</h6>
-          <p>{notes}</p>
-        </div>
-      )}
-
-      <div className={styles.row}>
-        <h6>Learn more</h6>
-        <a
-          href={`https://en.wikipedia.org/wiki/${cityName}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          Check out {cityName} on Wikipedia &rarr;
-        </a>
-      </div>
-
-      <div>
-        <BackButton />
-      </div>
-    </div>
-  );
+   );
 }
 
 export default City;
